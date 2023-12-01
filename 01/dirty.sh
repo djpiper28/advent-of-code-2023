dbe0484bdee8535f8daefdbb5dcc8d9e44403d9e
Danny Piper <danny.piper@bjss.com>
2023-12-01 14:59:24 +0000

    feat: added the worst bash script I have ever made
    This makes part 2 work

---
+++ 01/dirty.sh
@@ -0,0 +1,12 @@
+export file="input.txt"
+sed -i "s/one/1e/g" $file
+sed -i "s/two/2o/g" $file
+sed -i "s/three/3e/g" $file
+sed -i "s/four/4r/g" $file
+sed -i "s/five/5e/g" $file
+sed -i "s/six/6x/g" $file
+sed -i "s/seven/7n/g" $file
+sed -i "s/eight/8t/g" $file
+sed -i "s/nine/9e/g" $file
+make
+#./part2
